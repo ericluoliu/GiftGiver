f1636593a7cd31f26e2ae79cf70131b865cddb63
AlexHockett <alexhockett2@gmail.com>
2024-11-08 17:16:16 -0800

    Added backend functionality

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,15 @@
-from flask import Flask
+# API Key: AIzaSyCFSndRnJpXXIYpt_AykTrrPUIZ85cxTks
+
+from flask import Flask, request, jsonify
+import google.generativeai as genai
 
 app = Flask(__name__)
+genai.configure(api_key="AIzaSyCFSndRnJpXXIYpt_AykTrrPUIZ85cxTks")
+model = genai.GenerativeModel("gemini-1.5-flash")
 
-@app.route("/")
+@app.route("/", methods = ['POST'])
 def hello_world():
-    return "<p>Hello, World!</p>"
+    response = model.generate_content(request.data.get("input_query"))
+    return jsonify({"message": response.text})
+
+app.run()
